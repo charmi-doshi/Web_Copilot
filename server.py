0b2791cab1c7f9881b2ebb81a323307ae3436c33
ARPITHA NAGARAJ <arpithanagaraj@ARPITHAs-MacBook-Air.attlocal.net>
2025-03-30 17:47:37 -0700

    Changes from backend agenst to main

---
+++ server.py
@@ -0,0 +1,15 @@
+
+from fastapi import FastAPI
+from agents import  clinical_agent, food_agent
+
+app = FastAPI()
+
+@app.post("/query")
+async def route_query(query: dict):
+    agent_type = query.get('type')
+    if agent_type == "clinical":
+        return await clinical_rag_agent.process(query)
+    elif agent_type == "food":
+        return await food_security_agent.process(query)
+    else:
+        return await webui_agent.process(query)
